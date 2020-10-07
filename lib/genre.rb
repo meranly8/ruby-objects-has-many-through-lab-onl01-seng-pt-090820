e241e3d5166313089879489bc0be00466fc12313
Michelle Ranly <meranly8@gmail.com>
2020-10-07 03:40:39 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,4 +1,22 @@
 #Genre has many Songs; Genre has many Artists through Songs
 class Genre
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs 
+    
+  end
+  
+  def artists 
+    
+  end
   
 end
