feb91e8541181bc5264c3c2d961df421bbe243f6
Michelle Ranly <meranly8@gmail.com>
2020-10-07 03:34:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,24 @@
 #Artist has many Genres through Songs
+class Artist
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+  
+  # def songs
+  #   Song.all.select do |song|
+  #     song.artist == self
+  #   end
+  # end
+end
