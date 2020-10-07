565a8a836161ab7a7c55c78d2cd09ed711e21ea1
Michelle Ranly <meranly8@gmail.com>
2020-10-07 04:30:57 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,7 +1,7 @@
 #Patient has many Appointments; Patient has many Doctors through Appointments
 class Patient
   attr_accessor :name 
-  @@all = self 
+  @@all = [] 
   
   def initialize(name)
     @name = name 
